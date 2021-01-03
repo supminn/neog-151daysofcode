ace4a8969e4023a41f208bd0992d8abdd08847e7
supminn <30731236+supminn@users.noreply.github.com>
2021-01-03 20:12:12 +0530

    feat: web app version ready

--- src/App.js
+++ src/App.js
@@ -5,24 +5,30 @@ import './App.css';
 let startDate = new Date("01/01/2021"); 
 let todayDate = new Date();
 let diffInDays = Math.ceil((todayDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24));
-
+let maxTweetLen = 280;
 function App() {
   const [tweetMessage, tweetMsgHandler] = useState(`Day ${diffInDays} of #151daysofcode \n #neogcamp`);
   const [tweetSize, tweetSizeHandler] = useState(36);
+
+  const constFlow = (msgVal) =>{
+    // maxTweetLen = 280 - msgVal.length; // sort this logic
+    // after hashtags are choosen, we cannot type anymore - it refreshes the tweet
+    console.log(maxTweetLen);
+    tweetSizeHandler(msgVal.length);
+    tweetMsgHandler(msgVal);
+  }
 
   const msgHandler = (event) => {
     let msgTweet = `Day ${diffInDays} of #151daysofcode \n`;
     msgTweet += `${event.target.value}`;
     msgTweet += '\n#neogcamp';
-    tweetSizeHandler(msgTweet.length);
-    tweetMsgHandler(msgTweet);
+    constFlow(msgTweet);
   }
 
   const tagHandler = (event) => {
     let msgTweet = tweetMessage;
     msgTweet += ` ${event.target.innerText}`;
-    tweetSizeHandler(msgTweet.length);
-    tweetMsgHandler(msgTweet);
+    constFlow(msgTweet);
   }
   
   const tweetHandler = () => {
@@ -33,16 +39,26 @@ function App() {
 
   return (
     <div className="App">
-      <textarea placeholder="message" onChange={msgHandler}></textarea>
-      <button className="btn-tags" type="button" onClick={tagHandler}>#webdev</button>
-      <button className="btn-tags" type="button" onClick={tagHandler}>#JavaScript</button>
-      <button className="btn-tags" type="button" onClick={tagHandler}>#ReactJS</button>
-      <button className="btn-tags" type="button" onClick={tagHandler}>#codingisfun</button>
-      <button className="btn-tags" type="button" onClick={tagHandler}>#teamtanay</button>
-      <img src="twitter.svg" alt="Tweet" height='30' onClick={tweetHandler}/>
-      <h3>Preview of your tweet</h3>
+      <br/><br/>
+      <div className="data-section">
+        <br/>
+      <textarea className="txt-tweet" maxLength={maxTweetLen} placeholder="Enter the tweet message here" onChange={msgHandler}></textarea>
+      <br/>
+      <button className="btn-tag" type="button" onClick={tagHandler}>#webdev</button>
+      <button className="btn-tag" type="button" onClick={tagHandler}>#JavaScript</button>
+      <button className="btn-tag" type="button" onClick={tagHandler}>#ReactJS</button>
+      <button className="btn-tag" type="button" onClick={tagHandler}>#codingisfun</button>
+      <button className="btn-tag" type="button" onClick={tagHandler}>#teamtanay</button>
+      <br/><br/>
+      <span className="btn-tweet" onClick={tweetHandler}>Tweet <img src="twitter.svg" alt="Tweet"/></span>
+      <br/><br/>
+      <div className="data-section">
+      <h3>⬇ Preview of your tweet ⬇</h3>
       <pre className="txt-output">{tweetMessage}</pre>
-      <h4>{tweetSize}/280</h4>
+      <h4 className="limit-tweet">{tweetSize}/280</h4>
+      </div>
+      <br/>
+      </div>
     </div>
   );
 }
