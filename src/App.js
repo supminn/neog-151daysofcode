a32a301decf6bb5a039bd2f7ee008a8436b42d8a
supminn <30731236+supminn@users.noreply.github.com>
2021-01-07 19:56:42 +0530

    feat: customised start date

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import {useState} from "react";
 import './App.css';
 
-//Calculate the number of days from 1st Jan 2021
-let startDate = new Date("01/01/2021"); 
-let todayDate = new Date();
-let diffInDays = Math.ceil((todayDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24));
-//Calculating maxDate
-// var dd = todayDate.getDate();
-// var mm = todayDate.getMonth()+1;
-//  if(dd<10){
-//         dd='0'+dd
-//     } 
-//     if(mm<10){
-//         mm='0'+mm
-//     } 
-// let maxDate = todayDate.getFullYear()+'-'+mm+'-'+dd;
+  //Calculate the number of days from 1st Jan 2021
+  let startDate = new Date("01/01/2021"); 
+  let todayDate = new Date();
+  let diffInDays = Math.ceil((todayDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24));
+  //Calculating maxDate
+  var dd = todayDate.getDate();
+  var mm = todayDate.getMonth()+1;
+  if(dd<10){
+          dd='0'+dd
+  } 
+  if(mm<10){
+      mm='0'+mm
+  } 
+  let maxDate = todayDate.getFullYear()+'-'+mm+'-'+dd;
 
 function App() {
   const [tweetMessage, tweetMsgHandler] = useState(`Day ${diffInDays} of #151daysofcode\n`);
   const [tweetTag, tweetTagHandler] = useState('\n#neogcamp');
   const [tweetSize, tweetSizeHandler] = useState(35);
-
-  // const startDateHandler = (event) =>{
-  //   console.log(event.target.value);
-  //   startDate = new Date(event.target.value);
-  //   diffInDays = Math.ceil((todayDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24));
-  //   tweetMsgHandler(`Day ${diffInDays} of #151daysofcode`);
-  // }
+  const [dateValue, dateValueHandler] = useState("2021-01-01");
+  
+  const dateHandler =  (event) =>{
+    let value = event.target.value;
+    dateValueHandler(value);
+    startDate = new Date(value);
+    diffInDays = Math.ceil((todayDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24));
+    let changeDateText = {target:{value:""}};
+    msgHandler(changeDateText);
+  }
 
   const msgHandler = (event) => {
     let msgTweet = `Day ${diffInDays} of #151daysofcode \n`;
@@ -44,7 +47,7 @@ function App() {
     tweetSizeHandler(tweetMessage.length+msgTag.length);
   }
   
-  const tweetHandler = () => {
+  const sendTweet = () => {
     let finalMessage = encodeURIComponent(tweetMessage+tweetTag);
     let twitterUrl = `https://twitter.com/intent/tweet?text=${finalMessage}`;
     window.open(twitterUrl, "_blank");
@@ -52,11 +55,11 @@ function App() {
 
   return (
     <div className="App">
-      <h2>Tweet your daily progress of #151daysofcode challenge</h2>
+      <h2>Tweet your daily progress on #151daysofcode challenge</h2>
       <div className="data-section">
-        {/* <div>
-        Start date: <input type='date' value="2021-01-01" onChange={startDateHandler} min="2021-01-01" max={maxDate}></input>
-        </div> */}
+        <div>
+        Start date: <input className="inpt-startDate" type='date' value={dateValue} onChange={dateHandler} min="2021-01-01" max={maxDate}></input>
+        </div>
         <br/>
         <textarea className="txt-tweet" placeholder="Enter the tweet message here" onChange={msgHandler}></textarea>
         <br/>
@@ -77,7 +80,7 @@ function App() {
           <h4 className="limit-tweet">{tweetSize}/280</h4>
         </div>      
         <br/>
-        <span className="btn-tweet" onClick={tweetHandler}>Tweet <img src="twitter.svg" alt="Tweet"/></span>
+        <span className="btn-tweet" onClick={sendTweet}>Tweet <img src="twitter.svg" alt="Tweet"/></span>
         <br/>
       </div>
       <footer>
